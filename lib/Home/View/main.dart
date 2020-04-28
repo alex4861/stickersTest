be134a1ccdeb9c654715ff0bec21a6fbf47446cb
AlejandroOronzor <andres.rodriguezo@elektra.com.mx>
2020-04-28 18:34:09 -0500

    [Alex's log] feat: add dynamic themeApp and Tabbar

--- lib/Home/View/main.dart
+++ lib/Home/View/main.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
-
 import 'package:apptesting/BaseComponents/AppStandardBar.dart';
-import 'package:apptesting/BaseComponents/CupertinoStandardBar.dart';
 import 'package:apptesting/BaseComponents/SideMenu.dart';
 import 'package:apptesting/Home/View/ListItem.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -15,33 +14,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarColor: Colors.blue.shade700
+        statusBarColor: Theme.of(context).brightness == Brightness.light ? Colors.grey.shade700 : Colors.blue.shade700
     ));
+    debugPrint('${Theme.of(context).brightness}');
+    return new DynamicTheme(
+      defaultBrightness: Theme.of(context).brightness,
+        data: (brightness) =>
+        new ThemeData(
+          brightness: brightness,
+          primaryColor: Theme.of(context).brightness == Brightness.dark ? Colors.blue: null,
+          scaffoldBackgroundColor: Theme.of(context).brightness == Brightness.dark ? CupertinoColors.tertiarySystemGroupedBackground: null,
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Testing App'),
-      theme: ThemeData(
-        primaryColor: Platform.isIOS ? CupertinoColors.tertiarySystemBackground : Colors.blue,
-        scaffoldBackgroundColor: CupertinoColors.tertiarySystemGroupedBackground,
-      ),
-      darkTheme: ThemeData.dark(),
+        ),
+        themedWidgetBuilder: (context, theme) {
+          return new MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            home: MyHomePage(title: 'Testing App'),
+            theme: theme,
+          );
+        }
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,41 +47,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      drawer: SideMenu(),
-      body:
-          SafeArea(
-            bottom: false,
-            top: Platform.isAndroid,
-            child: CustomScrollView(
-              slivers: <Widget>[
-                AppStandardBar().build(context),
-                SliverList(delegate: SliverChildListDelegate([SafeArea(child:ListItem())]),)
-              ],
-            ),
-          )
-
+    return DefaultTabController(
+        length: 5,
+        child:
+        Scaffold(
+            drawer: SideMenu(context),
+            body:
+            SafeArea(
+              bottom: false,
+              top: Platform.isAndroid,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, value) {return [AppStandardBar(context, isCupertino: false,title: "Testing app",),];},
+                body: TabBarView(
+                  children: [
+                    ListItem(),
+                    Container(child: Center(child: Text("Nothing to show")),),
+                    Container(child: Center(child: Text("You have no favorites yet")),),
+                    Container(child: Center(child: Text("No recents files")),),
+                    Container(),
+                  ],
+                ),
+              ),
+            )
+        )
     );
   }
 
